63209de30f55b110122e7ac754e762e65e1e2de1
Tilman Holube <tilman@holube.de>
2024-01-11 22:16:09 +0100

    full window text editor

--- src/main/java/de/holube/noel/fx/view/MainView.java
+++ src/main/java/de/holube/noel/fx/view/MainView.java
@@ -1,15 +1,20 @@
 package de.holube.noel.fx.view;
 
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import lombok.Getter;
 
 @Getter
-public class MainView extends Pane {
+public class MainView extends AnchorPane {
 
     private final EditorView editorView;
 
     public MainView(EditorView editorView) {
         this.editorView = editorView;
+
+        AnchorPane.setTopAnchor(editorView, 0D);
+        AnchorPane.setLeftAnchor(editorView, 0D);
+        AnchorPane.setRightAnchor(editorView, 0D);
+        AnchorPane.setBottomAnchor(editorView, 0D);
         getChildren().add(editorView);
     }
 
